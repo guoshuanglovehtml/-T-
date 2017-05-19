7147a8062d0e1987d7348e3d02c5e96775a7a1bd
guoshuanglovehtml <1460840442@qq.com>
2017-05-19 17:56:17 +0800

    修改

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-$(document).ready(function () 
-{
-	// $("titles_bg").
-	var titles_bg = document.getElementsByClassName('titles_bg')[0];
-	console.log(titles_bg);
+window.onload = function () {
 
-	var title_contents = titles_bg.children;
-	console.log(title_contents);
-
-	// for (var i = 0; i < title_contents.length; i++) 
-	// {
-	// 	title_contents[i]
-	// }
-
-	$(".title_contents").each(function () 
-	{
-		$(this).animate({'top':'0px'},6000);
-	})
-
-});
+	// var i = 30 ;
+	// $(".title_contents").each(function () 
+	// {   
+	// 	i = i + 30 ;
+	// 	$(this).animate("top",i + "px");
+	// 	console.log(i);
+	// })
+};
